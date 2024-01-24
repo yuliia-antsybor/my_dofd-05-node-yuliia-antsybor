505c13d991ffe0ad03344ef559c228873736086e
Yuliia <alice1yulia@gmail.com>
2024-01-24 23:30:16 +0100

    creat docker file

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const CLIENT_PORT =
   cp_index !== -1 ? args[cp_index + 1] : process.env.CLIENT_PORT || 3000;
 
 const app = express();
+console.log(CLIENT_PORT);
 app.use(
   cors({
     origin: `http://localhost:${CLIENT_PORT}`,
